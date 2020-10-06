d562573367a56804525a31e9b3f461b83df9c769
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-10-05 23:44:44 -0400

    Add documentation for frontend forms

--- docs/.vuepress/config/locales/en/sidebar.js
+++ docs/.vuepress/config/locales/en/sidebar.js
@@ -1,89 +1,99 @@
 module.exports = {
-    '/extend/': [
-      {
-        title: 'Main Concepts',
-        collapsable: false,
-        children: [
-          '',
-          'start',
-          'frontend',
-          'routes',
-          'data',
-          'distribution',
-          'update-b8',
-          'update-b10',
-          'update-b12',
-          'update-b13',
-          'update-b14',
-        ]
-      },
-      {
-        title: 'Advanced Guides',
-        collapsable: false,
-        children: [
-          'authorization',
-          'console',
-          'formatting',
-          'i18n',
-          'mail',
-          'middleware',
-          'notifications',
-          'permissions',
-          'post-types',
-          'search',
-          'settings',
-          'testing',
-        ]
-      },
-      // {
-      //   title: 'Themes',
-      //   collapsable: false,
-      //   children: [
-      //     'theme',
-      //   ]
-      // },
-      // {
-      //   title: 'Localization',
-      //   collapsable: false,
-      //   children: [
-      //   ]
-      // }
-    ],
+  '/extend/': [
+    {
+      title: 'Main Concepts',
+      collapsable: false,
+      children: [
+        '',
+        'start',
+        'frontend',
+        'routes',
+        'data',
+        'distribution',
+        'update-b8',
+        'update-b10',
+        'update-b12',
+        'update-b13',
+        'update-b14',
+      ]
+    },
+    {
+      title: 'Reference Guides',
+      collapsable: false,
+      children: [
+        'frontend-pages',
+        'interactive-components',
+        'forms',
+        'backend-events'
+      ]
+    },
+    {
+      title: 'Advanced Guides',
+      collapsable: false,
+      children: [
+        'authorization',
+        'console',
+        'formatting',
+        'i18n',
+        'mail',
+        'middleware',
+        'notifications',
+        'permissions',
+        'post-types',
+        'search',
+        'settings',
+        'testing',
+      ]
+    },
+    // {
+    //   title: 'Themes',
+    //   collapsable: false,
+    //   children: [
+    //     'theme',
+    //   ]
+    // },
+    // {
+    //   title: 'Localization',
+    //   collapsable: false,
+    //   children: [
+    //   ]
+    // }
+  ],
 
-    '/': [
-      {
-        title: 'Introduction',
-        collapsable: false,
-        children: [
-          '',
-          'code-of-conduct',
-          'releases',
-          'contributing',
-          'bugs',
-          'faq'
-        ]
-      },
-      {
-        title: 'Setting Up',
-        collapsable: false,
-        children: [
-          'install',
-          'update',
-          'troubleshoot'
-        ]
-      },
-      {
-        title: 'Management',
-        collapsable: false,
-        children: [
-          'admin',
-          'config',
-          'extensions',
-          'languages',
-          'themes',
-          'mail',
-          'console'
-        ]
-      }
-    ],
-  }
+  '/': [
+    {
+      title: 'Introduction',
+      collapsable: false,
+      children: [
+        '',
+        'code-of-conduct',
+        'releases',
+        'contributing',
+        'bugs',
+        'faq'
+      ]
+    },
+    {
+      title: 'Setting Up',
+      collapsable: false,
+      children: [
+        'install',
+        'update',
+        'troubleshoot'
+      ]
+    },
+    {
+      title: 'Management',
+      collapsable: false,
+      children: [
+        'admin',
+        'config',
+        'extensions',
+        'languages',
+        'themes',
+        'mail',
+        'console'
+      ]
+    }
+  ],
+}
