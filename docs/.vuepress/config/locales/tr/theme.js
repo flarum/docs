f34a3763e908e34434018b1a290c27d9d70ee3b4
DursunCanPoyraz <info@dursuncanpoyraz.com.tr>
2020-12-21 16:49:53 +0300

    Settings have been made for Turkish (Türkçe).

---
+++ docs/.vuepress/config/locales/tr/theme.js
@@ -0,0 +1,10 @@
+module.exports = {
+    selectText: 'Diller',
+    label: 'Türkçe',
+    
+    editLinkText: 'Bu sayfayı düzenle',
+    lastUpdated: 'Son güncelleme',
+
+    nav: require('./nav'),
+    sidebar: require('./sidebar'),
+}
