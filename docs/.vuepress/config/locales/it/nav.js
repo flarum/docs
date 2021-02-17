ce850346e4fabcc7c9a8f761a2f6362498b20278
Justoverclock <info@justoverclock.it>
2021-02-17 10:29:11 +0100

    Added vuepress file
    vuepress file translated

---
+++ docs/.vuepress/config/locales/it/nav.js
@@ -0,0 +1,13 @@
+module.exports = [
+    { text: 'Guide', link: '/' },
+    { text: 'Extend', link: '/extend/' },
+    { text: 'Referenze API', link: 'https://api.docs.flarum.org/' },
+    {
+      text: 'Flarum',
+      items: [
+        { text: 'Home', link: 'https://flarum.org/' },
+        { text: 'Community', link: 'https://discuss.flarum.org/' },
+        { text: 'GitHub', link: 'https://github.com/flarum/flarum/' },
+      ]
+    }
+  ]
---
+++ docs/.vuepress/config/locales/it/sidebar.js
@@ -0,0 +1,104 @@
+module.exports = {
+  '/extend/': [
+    {
+      title: 'Concetti principali',
+      collapsable: false,
+      children: [
+        '',
+        'start',
+        'frontend',
+        'routes',
+        'data',
+        'distribution',
+        'update-b8',
+        'update-b10',
+        'update-b12',
+        'update-b13',
+        'update-b14',
+        'update-b15',
+      ]
+    },
+    {
+      title: 'Guide di riferimento',
+      collapsable: false,
+      children: [
+        'admin',
+        'frontend-pages',
+        'interactive-components',
+        'forms',
+        'backend-events',
+      ]
+    },
+    {
+      title: 'Guide Avanzate',
+      collapsable: false,
+      children: [
+        'api-throttling',
+        'authorization',
+        'console',
+        'formatting',
+        'i18n',
+        'mail',
+        'middleware',
+        'slugging',
+        'notifications',
+        'permissions',
+        'post-types',
+        'search',
+        'service-provider',
+        'settings',
+        'testing',
+      ]
+    },
+    // {
+    //   title: 'Themes',
+    //   collapsable: false,
+    //   children: [
+    //     'theme',
+    //   ]
+    // },
+    // {
+    //   title: 'Localization',
+    //   collapsable: false,
+    //   children: [
+    //   ]
+    // }
+  ],
+
+  '/': [
+    {
+      title: 'Introduzione',
+      collapsable: false,
+      children: [
+        '',
+        'code-of-conduct',
+        'releases',
+        'contributing',
+        'bugs',
+        'faq'
+      ]
+    },
+    {
+      title: 'Impostazioni',
+      collapsable: false,
+      children: [
+        'install',
+        'update',
+        'troubleshoot'
+      ]
+    },
+    {
+      title: 'Gestione',
+      collapsable: false,
+      children: [
+        'admin',
+        'config',
+        'extensions',
+        'languages',
+        'themes',
+        'mail',
+        'console'
+      ]
+    }
+  ],
+}
