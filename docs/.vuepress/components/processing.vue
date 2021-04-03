f151e63dbcc6fc604bbb3bdc3f180c2d664a9ea8
Golden <35162802+Littlegolden@users.noreply.github.com>
2021-04-03 12:58:56 +0800

    Revert changes of directory structure

--- docs/.vuepress/components/processing.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="notification-container">
-        <div class="processing-tip">
-            <p>⚡当前文档仍在翻译中（进度 6/23），有关最新信息，请查阅 <a href="/extend/" target="_blank" rel="noopener noreferrer">英文文档</a>。如果翻译有问题，请 <a href="https://github.com/flarum/docs/issues" target="_blank" rel="noopener noreferrer">告诉我们</a>。</p>
-        </div>
-    </div>
-</template>
