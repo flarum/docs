07d15e0a627faa0bce7ca3d0c83d1ec0a2e38fc1
Sami Mazouz <sychocouldy@gmail.com>
2024-01-12 14:51:21 +0100

    feat: package manager & installable packages (#469)
    * feat: package manager & installable packages
    
    * chore: 1.x
    
    * docs: configuring repositories
    
    * chore
    
    * chore: rename to extension manager
    
    * chore: rc (release candidate)
    
    * chore: update
    
    * docs: extension manager installation instructions

--- sidebars.js
+++ sidebars.js
@@ -150,7 +150,7 @@ module.exports = {
         'internal/README',
         'internal/merging-policy',
         'internal/bundled-extensions-policy',
-        'internal/package-manager',
+        'internal/extension-manager',
       ]
     },
   ]
